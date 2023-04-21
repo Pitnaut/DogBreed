ba4c0d172981cdd1b635e810657412c4243520cc
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-04-21 12:54:00 +0200

    Add new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import footsprint from './assets/huella.png'
 
 function App() {
 
   return (
-    <div className="App">    
+    <div className="App">
+      <header className="header">
+        <img className="huella" src={footsprint} />
+        <h1 className="titulo">Enciclopedia Perruna</h1>  
+      </header>    
     </div>
   )
 }
